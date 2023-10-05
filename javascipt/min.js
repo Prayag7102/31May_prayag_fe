b2f3da4d7bfc064e45f3fe6b2d2968355172ee64
Prayag7102 <131190536+Prayag7102@users.noreply.github.com>
2023-10-05 13:21:04 +0530

    Add files via upload

---
+++ javascipt/min.js
@@ -0,0 +1,10 @@
+var a= 12;
+var b= 20;
+
+document.writeln(++a + "<br>");
+document.writeln(++b + "<br>");
+document.writeln(a+b + "<br>");
+document.writeln(a-b + "<br>");
+document.writeln(a*b + "<br>");
+document.writeln(a/b + "<br>");
+document.writeln(a % b );
